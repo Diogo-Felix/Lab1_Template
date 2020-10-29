10e3b1ee7b8bc77eb0f5dc861559f42bf94130a9
Diogo-Felix <undisclosed>
2020-10-29 21:29:41 +0000

    b

--- src/pt/pa/Main.java
+++ src/pt/pa/Main.java
@@ -7,7 +7,7 @@ public class Main {
 
     public static void main(String[] args) {
 
-        Queue<Integer> queue = new QueueLinkedList<>(10);
+        Queue<Integer> queue = new QueueLinkedList<>(5);
 
         //TODO: testar implementação
 
